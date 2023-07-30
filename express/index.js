e8fa1c6ed4c5e0b3b90352caa26a445f29938348
ayaniegain <ayaniegain@gmail.com>
2023-07-30 18:59:06 +0530

    feat : express js

--- express/index.js
+++ express/index.js
@@ -1,17 +1,23 @@
 const express= require("express")
+const path =require("path")
 const app=express()
 const port=3000
 
+let pathDir=path.join(__dirname,'public')
+app.use(express.static(pathDir))
+
+
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.sendFile(`${pathDir}/home.html`)
 })
 
 app.get("/about",(req,res)=>{
-    res.send("hello world from about")
+    res.sendFile(`${pathDir}/about.html`)
 })
 
 app.get("/download",(req,res)=>{
-    res.send("hello world from download")
+    res.download(`${pathDir}/task.pdf`)
+
 })
 
 app.listen(port,()=>{
